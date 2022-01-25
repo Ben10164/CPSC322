fb7b171a23e3f5080e2f1e72c69e0260b72f27f6
Ben Puryear <bpuryear@zagmail.gonzaga.edu>
2022-01-25 12:03:53 -0800

    test

---
+++ U2-Python/PythonBasicsFun/unit_tests.py
@@ -0,0 +1,10 @@
+# ASSERT
+
+# assert basically is a function that checks if the condition is true or not.
+# if the condition is true, then the program will continue to run.
+# if the condition is false, then the program will stop and print the error message.
+
+assert 3 == 3, "3 is not equal to 3"
+print("It past the first assert!")
+assert 3 == 5, "3 is not equal to 5"
+print("It wont get here :(")
