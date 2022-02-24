0ea7b11cf5dae904c486956c75dde36dd570f803
Ben Puryear <bpuryear@zagmail.gonzaga.edu>
2022-02-24 11:50:16 -0800

    2.24.22

--- U4-Supervised-Learning/ClassificationFun/kNN_in_class.py
+++ U4-Supervised-Learning/ClassificationFun/kNN_in_class.py
@@ -2,6 +2,7 @@ import utils
 import operator
 import numpy as np
 from scipy.spatial import distance
+from sklearn.neighbors import KNeighborsClassifier
 
 def main():
     header = ["att1", "att2"]
@@ -21,15 +22,9 @@ def main():
 
     X_test = [2, 3]
 
-    row_indexes_distances = []
-    for i, train_instance in enumerate(X_train):
-        dist = compute_euclidean_distance(train_instance, X_test)
-        row_indexes_distances.append([i, dist])
+    row_indexes_distances = get_row_indexes_distances_sorted(X_train, X_test)
 
-    print(row_indexes_distances)
-
-    # now we can sort the items by the distance (item[1])
-    row_indexes_distances.sort(key=operator.itemgetter(-1))
+    # print the closest 3 (k=3)
     k = 3
     top_k = row_indexes_distances[:k]
     print("Top k:")
@@ -39,8 +34,17 @@ def main():
     # TODO: extract the y lables from y_train for the k bnearest neightbors
     # then figure out the majority class labvel, that is this
     # test instance's rediction
-    freqs = utils.get_frequencies(y_train )
-    print(freqs)
+    
+    # freqs = utils.get_frequencies(y_train )
+    # print(freqs)
+
+    test_instance = [2,3]
+
+    knn_clf = KNeighborsClassifier(n_neighbors=3) # n_neighbors=k=3
+    knn_clf.fit(X_train, y_train)
+    # kneighbors returns a tuple of a numpy array of distances and a numpy array of labels NOTE: PARALLEL
+    print(knn_clf.kneighbors([test_instance])) # returns the same thing as our function
+
 
 
 
@@ -63,6 +67,19 @@ def compute_euclidean_distance(v1, v2):
         total += (v1[i] - v2[i]) ** 2
     return (total ** (1/2))
 
+
+def get_row_indexes_distances_sorted(X_train, X_test):
+    row_indexes_distances = []
+    for i, train_instance in enumerate(X_train):
+        dist = compute_euclidean_distance(train_instance, X_test)
+        row_indexes_distances.append([i, dist])
+
+    # print(row_indexes_distances)
+
+    # now we can sort the items by the distance (item[1])
+    row_indexes_distances.sort(key=operator.itemgetter(-1))
+
+    return row_indexes_distances
 
 if __name__ == '__main__':
     main()
